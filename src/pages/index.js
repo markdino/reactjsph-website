7cbcbaed9b135a7b636a072c3dc0cb31a49939c0
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2019-10-02 22:09:15 +0800

    ✨ Create Mission Statement

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from "react"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
 import Hero from "../components/hero"
+import MissionSection from "../components/MissionSection"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
+    <MissionSection />
   </Layout>
 )
 
