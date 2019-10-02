fc27cad1fb97d599f95cd1505023c1fa417385f9
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2019-10-03 00:23:29 +0800

    ✨ Create Vision Statement

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/hero"
 import MissionSection from "../components/MissionSection"
+import VisionSection from "../components/VisionSection"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
     <MissionSection />
+    <VisionSection />
   </Layout>
 )
 
